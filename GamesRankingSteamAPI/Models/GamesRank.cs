bf1807d21dcca2ac3800b79afbde5def162a699c
padii <chudy3221@gmail.com>
2020-11-01 22:23:42 +0100

    Some necessary changes
    +Added files for Database connectivity (in the future);
    +Added "About" page;
    +Added .css file controlling the color of font
    *Changed the looks of the site;
    *Changed style of Navigation bar;
    -Deleted useless file;

---
+++ GamesRankingSteamAPI/Models/GamesRank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mysqlefcore
+{
+    public class Games
+    {
+        public Games(){}
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string wwwaddress { get; set; }
+        public int PEGI { get; set; }
+        public int Popularity_Points { get; set; }
+
+        public GameInfo FK_GamesIDs { get; set; }
+    }
+
+    public class Genres
+    {
+        public Genres(){}
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public GameInfo FK_GenresIDs { get; set; }
+    }
+
+    public class GameInfo {
+        public GameInfo()
+        {
+            this.Collection_Games = new HashSet<Games>();
+            this.Collection_Genres = new HashSet<Genres>();
+        }
+        public virtual ICollection<Games> Collection_Games { get; set; }
+        public virtual ICollection<Genres> Collection_Genres { get; set; }
+    }
+}
---
+++ GamesRankingSteamAPI/Models/GamesRankContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using System.Data.Entity;
+
+namespace mysqlefcore
+{
+    public class GamesRankContext : DbContext
+    {
+        public DbSet<Games> Games { get; set; }
+        public DbSet<Genres> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=GameRankDbase;user=user;password=password");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Games>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Title).IsRequired();
+            });
+
+            modelBuilder.Entity<Genres>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<GameInfo>(entity =>
+            {
+                entity.HasMany(d => d.Collection_Games)
+                      .WithOne(p => p.FK_GamesIDs);
+                entity.HasMany(d => d.Collection_Genres)
+                      .WithOne(d => d.FK_GenresIDs);
+            });
+        }
+    }
+}
