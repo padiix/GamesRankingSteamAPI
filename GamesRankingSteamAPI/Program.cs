a126d4840d072ea7d19a64bbae2d1e50da964776
padii <chudy3221@gmail.com>
2020-11-07 23:36:42 +0100

    Changed looks of Index and some
    +Added chart (NOT WORKING FOR NOW)
    *Changed Index

--- GamesRankingSteamAPI/Program.cs
+++ GamesRankingSteamAPI/Program.cs
@@ -18,7 +18,6 @@ namespace GamesRankingSteamAPI
             CreateHostBuilder(args).Build().Run();
         }
 
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
