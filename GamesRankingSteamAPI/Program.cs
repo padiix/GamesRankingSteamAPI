e045e35439a51ddf2c109e660ad7f4bd11ee3474
padii <chudy3221@gmail.com>
2020-11-07 22:08:45 +0100

    Deletion of db models
    This branch will focus on site looks

--- GamesRankingSteamAPI/Program.cs
+++ GamesRankingSteamAPI/Program.cs
@@ -16,53 +16,8 @@ namespace GamesRankingSteamAPI
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            InsertData();
-            PrintData();
         }
 
-        private static void InsertData()
-        {
-            using (var context = new LibraryContext())
-            {
-                // Creates the database if not exists
-                context.Database.EnsureCreated();
-
-                // Adds a publisher
-                var publisher = new Publisher
-                {
-                    Name = "Mariner Books"
-                };
-                context.Publisher.Add(publisher);
-
-                // Adds some books
-                context.Book.Add(new Book
-                {
-                    ISBN = "978-0544003415",
-                    Title = "The Lord of the Rings",
-                    Author = "J.R.R. Tolkien",
-                    Language = "English",
-                    Pages = 1216,
-                    Publisher = publisher
-                });
-                context.Book.Add(new Book
-                {
-                    ISBN = "978-0547247762",
-                    Title = "The Sealed Letter",
-                    Author = "Emma Donoghue",
-                    Language = "English",
-                    Pages = 416,
-                    Publisher = publisher
-                });
-
-                // Saves changes
-                context.SaveChanges();
-            }
-        }
-
-        private static void PrintData()
-        {
-            throw new NotImplementedException();
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
