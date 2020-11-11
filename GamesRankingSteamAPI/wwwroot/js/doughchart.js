d8e6422f0bab9e584818581341c298e403adf403
padii <chudy3221@gmail.com>
2020-11-11 22:21:12 +0100

    Modified last commit
    (*) Repaired chart

--- GamesRankingSteamAPI/wwwroot/js/doughchart.js
+++ GamesRankingSteamAPI/wwwroot/js/doughchart.js
@@ -1,31 +1,34 @@
-﻿var config = {
+﻿var ctx = document.getElementById('myChart');
+
+var myChart = new Chart(ctx, {
     type: 'doughnut',
+
     data: {
-        datasets: {
-            datasets: [{
-                data: [
-                    5, 10, 15, 20, 25, 30, 35, 40, 45, 50
-                ],
-                backgroundColor: [
-                        '#1f77b4',
-                        '#ff7f0e',
-                        '#2ca02c',
-                        '#d62728',
-                        '#96E90A',
-                        '#7D4721',
-                        '#2AF5B3',
-                        '#335268',
-                        '#7BECF7',
-                        '#37A62E'
-                ],
-                label: "liczba grających osób",
-            }],
-            labels: [
-                "Gra1", "Gra2", "Gra3", "Gra4", "Gra5", "Gra6", "Gra7", "Gra8", "Gra9", "Gra10"
-            ]
-        }
+        labels: [
+            "Gra1", "Gra2", "Gra3", "Gra4", "Gra5", "Gra6", "Gra7", "Gra8", "Gra9", "Gra10"
+        ],
+        datasets: [{
+            label: "liczba grających osób",
+            data: [
+                5, 10, 15, 20, 25, 30, 35, 40, 45, 50
+            ],
+            backgroundColor: [
+                '#1f77b4',
+                '#ff7f0e',
+                '#2ca02c',
+                '#d62728',
+                '#96E90A',
+                '#7D4721',
+                '#2AF5B3',
+                '#335268',
+                '#7BECF7',
+                '#2ca12d'
+            ],
+            borderColor: '#343a40'
+        }]
     },
-    options: {
+
+    /*options: {
         responsive: true,
         legend: {
             position: 'bottom',
@@ -43,11 +46,11 @@
             animateScale: true,
             animateRotate: true
         }
-    }
-};
+    }*/
+    //TODO: NAPRAWIĆ WYŚWIETLANIE WYKRESU.
+});
+    
 
-window.onload = function () {
-    var ctx = document.getElementById('doughnut-chart').getContext('2d');
-    window.myDoughnut = new Chart(ctx, config);
-};
+
+
 
